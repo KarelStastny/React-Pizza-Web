26c751d74500cd7e5d4f181820bd823519806354
Karel Šťastný <karell29@icloud.com>
2023-06-26 09:25:38 +0200

    Dodělána strana galerie a o nás

--- src/pages/Galerie.js
+++ src/pages/Galerie.js
@@ -1,12 +1,57 @@
-
+import ImageGallery from 'react-image-gallery';
 import React from 'react'
+import "react-image-gallery/styles/scss/image-gallery.scss"
+import "./Galerie.scss"
+
+import img1 from "../images/pizza1.jpg"
+import img2 from "../images/pizza2.jpg"
+import img3 from "../images/pizza3.jpg"
+import img4 from "../images/pizza4.jpg"
+import img5 from "../images/pizza5.jpg"
+import img6 from "../images/pizza6.jpg"
 
 const Galerie = () => {
+
+
+
   return (
-    <div>
-      
+    <div className='gallery-slider'>
+      <h1>Naše galerie</h1>
+
+      <ImageGallery items={images} 
+        slideInterval={500}
+      />
     </div>
   )
 }
 
 export default Galerie
+
+const images = [
+  {
+    original: img1,
+    thumbnail: img1,
+  },
+  {
+    original: img2,
+    thumbnail: img2,
+  },
+  {
+    original: img3,
+    thumbnail: img3,
+  },
+  {
+    original: img4,
+    thumbnail: img4,
+  },
+  {
+    original: img5,
+    thumbnail: img5,
+  },
+  {
+    original: img6,
+    thumbnail: img6,
+  },
+
+];
+
--- src/pages/ONas.js
+++ src/pages/ONas.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import "./ONas.scss"
 
 const ONas = () => {
   return (
-    <div>
-      sefef
+    <div className='o-nas-page'>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga magnam doloribus dolorum eaque autem, blanditiis incidunt dicta maiores adipisci voluptatem consequuntur quos sed eveniet eligendi cum itaque a nam maxime!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi provident odit perspiciatis excepturi at voluptatum, labore culpa cumque ab, ad nostrum accusamus molestiae. Aut mollitia reprehenderit, in nam ea sint.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum distinctio totam facilis placeat autem, inventore eaque laboriosam maiores minima eveniet at cupiditate expedita, explicabo odit enim velit asperiores quis excepturi?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa perspiciatis quibusdam est doloremque, laudantium unde eum ad blanditiis dolores nemo molestiae fugiat dolore totam! Nulla eaque deserunt minima nam vitae?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, eos velit nam ex quos consequatur quam atque natus exercitationem obcaecati hic dolorum cum sunt magni vel! Ratione iusto placeat laboriosam.
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, nemo labore illo laboriosam eum deserunt ea, inventore, cum voluptatem temporibus reiciendis. Dicta eveniet possimus nihil et, inventore modi. Reprehenderit, nostrum.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid corporis delectus eos architecto. Adipisci nemo necessitatibus ipsa ratione maxime aliquid et? Velit omnis facere nulla molestias consequatur labore animi? Quia.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt et tenetur quas architecto. Sapiente error, repudiandae possimus culpa quas eius. Deleniti impedit, voluptas quos modi beatae illo expedita in optio!
     </div>
   )
 }
